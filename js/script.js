9574153c7b8ed7734efe94b05307306b3f64f5c0
softmipc <74118883+softmipc@users.noreply.github.com>
2020-11-07 17:38:01 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){ 	
+						   
+						   
+	// radius Box
+//	$('.social-links a').css({"border-radius": "4px", "-moz-border-radius":"4px", "-webkit-border-radius":"4px"});
+//	$('.wp-pagenavi a').css({"border-radius": "4px", "-moz-border-radius":"4px", "-webkit-border-radius":"4px"});
+//	$('.wp-pagenavi .current').css({"border-radius": "4px", "-moz-border-radius":"4px", "-webkit-border-radius":"4px"});
+	// end radius Box
+
+    // add tags span to a href
+//    $('.topnav ul').children('li').each(function() {
+//      $(this).children('a').html('<span>'+$(this).children('a').text()+'</span>'); // add tags span to a href
+//    });
+//    $('#nav1 ul').children('li').each(function() {
+//      $(this).children('a').html('<span>'+$(this).children('a').text()+'</span>'); // add tags span to a href
+//    });
+	// end add tags span to a href
+	
+	$("ul.sf-menu").superfish({
+		autoArrows:  false,
+		delay:       400,                             // one second delay on mouseout 
+		animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
+		speed:       'fast',                          // faster animation speed 
+		autoArrows:  false,                           // disable generation of arrow mark-up 
+		dropShadows: false                            // disable drop shadows 			
+	}); 
+	
+	$('ul.list li').each(function() {
+	  var a = $(this).children('a');
+	  var aClass = a.attr('rel');
+	  if (a.hasClass('active')) {     
+	   $('.'+aClass).css({'display':'block'});
+	  } else {
+	   $('.'+aClass).css({'display':'none'});
+	  }
+	 });
+	 $('ul.list li a').click(function () {             
+	  var thisaClass = $(this).attr('rel');
+	  $(this).parent('li').parent('ul').children('li').each(function() {
+	   var a = $(this).children('a');
+	   var aClass = a.attr('rel');
+	   if (thisaClass == aClass) {
+		$('.'+aClass).show();
+		a.attr('class','active');
+	   } else {
+		$('.'+aClass).hide();
+		a.attr('class','');
+	   }
+	  });
+	  return false;
+	});
+	 
+	 $("#slider").easySlider({
+		controlsBefore:	'<p id="controls">',
+		controlsAfter:	'</p>',
+		auto: true, 
+		continuous: true
+	});	
+	 
+	 
+});	
